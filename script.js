85f6686e2221f7c651ed83ed707dcb5589696578
term <termissues@gmail.com>
2023-08-06 23:23:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const fullscreenMenu = document.querySelector('.fullscreen__menu');
 const closeBtn = document.querySelector('.close__button');
 const header = document.querySelector('.header');
 
-
 navbarBurger.addEventListener('click', () => {
   fullscreenMenu.style.display = 'block';
   header.style.display = 'none';
@@ -12,4 +11,53 @@ navbarBurger.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   fullscreenMenu.style.display = 'none';
   header.style.display = 'flex';
+});
+
+const moreButton = document.getElementById('moreButton');
+const dropdownMenu = document.querySelector('.dropdown__menu');
+
+moreButton.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('show');
+});
+
+document.addEventListener('click', (event) => {
+  if (!event.target.matches('#moreButton')) {
+    dropdownMenu.classList.remove('show');
+  }
+});
+
+let isIconRotated = false;
+
+document.addEventListener('DOMContentLoaded', function () {
+  const moreButton = document.getElementById('moreButton');
+  const icon = moreButton.querySelector('i');
+
+  moreButton.addEventListener('click', function () {
+    icon.classList.toggle('rotate180');
+    isIconRotated = !isIconRotated;
+  });
+
+  document.addEventListener('click', function (event) {
+    if (!event.target.matches('#moreButton') && isIconRotated) {
+      icon.classList.remove('rotate180');
+      isIconRotated = false;
+    }
+  });
+});
+
+document.addEventListener('click', function(event) {
+  const clickedElement = event.target;
+
+  let elementWithStartId = clickedElement.closest('[id^="start-"]');
+
+  if (elementWithStartId) {
+      const idValue = elementWithStartId.getAttribute('id');
+      const tempInput = document.createElement('input');
+
+      tempInput.value = `termjs.github.io/codeakademis/mypath.html#`+idValue;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      document.execCommand('copy');
+      document.body.removeChild(tempInput);
+  }
 });
